f88d1a610ffe003178b3fdaf806cbe598b71f881
Petar Maymounkov <petarm@gmail.com>
2022-10-27 10:52:44 -0700

    add to mail test

--- test/id/mail_test.go
+++ test/id/mail_test.go
@@ -22,14 +22,16 @@ func TestSendReceive(t *testing.T) {
 	svc0 := id.IdentityService{IdentityConfig: testCommunity.UserIdentityConfig(0)}
 	svc1 := id.IdentityService{IdentityConfig: testCommunity.UserIdentityConfig(0)}
 
-	testMsg := "hello world"
+	testMsg := []string{"a", "b", "c"}
 	testTopic := "topic"
 
-	_, err = svc0.SendMail(ctx, &id.SendMailIn{Topic: testTopic, Message: testMsg})
+	// send msg 1
+	_, err = svc0.SendMail(ctx, &id.SendMailIn{Topic: testTopic, Message: testMsg[0]})
 	if err != nil {
 		t.Fatal(err)
 	}
 
+	// receive msg 1
 	receiveOut, err := svc1.ReceiveMail(ctx,
 		&id.ReceiveMailIn{
 			SenderRepo: testCommunity.UserPublicRepoURL(0),
@@ -43,7 +45,41 @@ func TestSendReceive(t *testing.T) {
 		t.Fatalf("expecting 1 message, got %v", len(receiveOut.Messages))
 	}
 
-	if receiveOut.Messages[0] != testMsg {
-		t.Fatalf("expecting %v, got %v", testMsg, receiveOut.Messages[0])
+	if receiveOut.Messages[0] != testMsg[0] {
+		t.Fatalf("expecting %v, got %v", testMsg[0], receiveOut.Messages[0])
 	}
+
+	// send msg 2, 3
+	_, err = svc0.SendMail(ctx, &id.SendMailIn{Topic: testTopic, Message: testMsg[1]})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = svc0.SendMail(ctx, &id.SendMailIn{Topic: testTopic, Message: testMsg[2]})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// receive msg 2, 3
+	receive23Out, err := svc1.ReceiveMail(ctx,
+		&id.ReceiveMailIn{
+			SenderRepo: testCommunity.UserPublicRepoURL(0),
+			Topic:      testTopic,
+		})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(receive23Out.Messages) != 2 {
+		t.Fatalf("expecting 2 messages, got %v", len(receive23Out.Messages))
+	}
+
+	if receive23Out.Messages[0] != testMsg[1] {
+		t.Fatalf("expecting %v, got %v", testMsg[1], receive23Out.Messages[0])
+	}
+
+	if receive23Out.Messages[1] != testMsg[2] {
+		t.Fatalf("expecting %v, got %v", testMsg[2], receive23Out.Messages[1])
+	}
+
 }

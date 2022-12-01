7ab985fb4a3182fd2cc20ba1dcfd5214b55440fd
Petar Maymounkov <petarm@gmail.com>
2022-12-01 08:33:27 -0800

    first draft github action for cron tallying

---
+++ .scripts/build-and-tally.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# run this script from the root of the gov4git repo
+
+cd gov4git
+go build
+
+mkdir -p ~/.gov4git
+
+cat <<EOF >> ~/.gov4git/config.json
+{
+     "auth" : {
+          "git@github.com:gov4git/gov4git.git": { "access_token": "${{ secrets.GITHUB_TOKEN }}" },
+          "git@github.com:gov4git/gov4git.private.git": { "access_token": "${{ secrets.GITHUB_TOKEN }}" },
+     },
+     "gov_public_url": "git@github.com:gov4git/gov4git.git",
+	"gov_public_branch": "gov",
+	"gov_private_url": "git@github.com:gov4git/gov4git.private.git",
+	"gov_private_branch": "gov",
+}
+EOF
+echo ~/.gov4git/config.json

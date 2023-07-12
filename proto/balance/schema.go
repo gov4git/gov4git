acd791f35b8807091765d051068dca3a14914222
Petar Maymounkov <petarm@gmail.com>
2023-07-12 13:15:05 -0700

    do not allow backslashes in balance names

--- proto/balance/schema.go
+++ proto/balance/schema.go
@@ -1,12 +1,13 @@
 package balance
 
 import (
+	"path/filepath"
 	"strings"
 )
 
 type Balance string
 
 func userPropKey(balanceKey Balance) string {
-	key := strings.TrimLeft(string(balanceKey), "/")
+	key := strings.TrimLeft(filepath.ToSlash(string(balanceKey)), "/")
 	return strings.Join([]string{"balance", key}, "/")
 }

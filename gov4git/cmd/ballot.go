5daf0ea079c620f06401bad41d0722ad891dd057
Petar Maymounkov <petarm@gmail.com>
2023-08-18 12:15:03 -0700

    fix default

--- gov4git/cmd/ballot.go
+++ gov4git/cmd/ballot.go
@@ -218,7 +218,7 @@ func init() {
 	// list
 	ballotCmd.AddCommand(ballotListCmd)
 	ballotListCmd.Flags().BoolVar(&ballotOnlyNames, "only_names", false, "list only ballot names")
-	ballotListCmd.Flags().BoolVar(&ballotOnlyOpen, "open", true, "list only open ballots")
+	ballotListCmd.Flags().BoolVar(&ballotOnlyOpen, "open", false, "list only open ballots")
 	ballotListCmd.Flags().BoolVar(&ballotOnlyClosed, "closed", false, "list only closed ballots")
 	ballotListCmd.Flags().BoolVar(&ballotOnlyFrozen, "frozen", false, "list only frozen ballots")
 	ballotListCmd.Flags().StringVar(&ballotWithParticipant, "participant", "", "list only ballots where the given user is a participant")

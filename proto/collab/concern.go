492eed158812b8054681ac20b91b682b623450ae
Petar Maymounkov <petarm@gmail.com>
2023-08-23 13:47:15 -0700

    rename issue -> concern

---
+++ proto/collab/concern.go
@@ -0,0 +1,50 @@
+package collab
+
+import (
+	"context"
+
+	"github.com/gov4git/gov4git/proto"
+	"github.com/gov4git/gov4git/proto/gov"
+	"github.com/gov4git/lib4git/git"
+	"github.com/gov4git/lib4git/must"
+)
+
+func IsConcern(ctx context.Context, addr gov.GovAddress, name ConcernName) bool {
+	return IsConcernLocal(ctx, gov.Clone(ctx, addr).Tree(), name)
+}
+
+func IsConcernLocal(ctx context.Context, t *git.Tree, name ConcernName) bool {
+	err := must.Try(func() { concernKV.Get(ctx, concernNS, t, name) })
+	return err == nil
+}
+
+func OpenConcern(ctx context.Context, addr gov.GovAddress, name ConcernName, trackerURL string) {
+	cloned := gov.Clone(ctx, addr)
+	chg := OpenConcernStageOnly(ctx, cloned.Tree(), name, trackerURL)
+	proto.Commit(ctx, cloned.Tree(), chg)
+	cloned.Push(ctx)
+}
+
+func OpenConcernStageOnly(ctx context.Context, t *git.Tree, name ConcernName, trackerURL string) git.ChangeNoResult {
+	must.Assert(ctx, !IsConcernLocal(ctx, t, name), ErrConcernAlreadyExists)
+	state := ConcernState{
+		Name:       name,
+		TrackerURL: trackerURL,
+		Closed:     false,
+	}
+	return concernKV.Set(ctx, concernNS, t, name, state)
+}
+
+func CloseConcern(ctx context.Context, addr gov.GovAddress, name ConcernName) {
+	cloned := gov.Clone(ctx, addr)
+	chg := CloseConcernStageOnly(ctx, cloned.Tree(), name)
+	proto.Commit(ctx, cloned.Tree(), chg)
+	cloned.Push(ctx)
+}
+
+func CloseConcernStageOnly(ctx context.Context, t *git.Tree, name ConcernName) git.ChangeNoResult {
+	state := concernKV.Get(ctx, concernNS, t, name)
+	must.Assert(ctx, !state.Closed, ErrConcernAlreadyClosed)
+	state.Closed = true
+	return concernKV.Set(ctx, concernNS, t, name, state)
+}

75ffaff4edb82a6ad474010ea692cef97b3e9ab8
Petar Maymounkov <petarm@gmail.com>
2023-08-23 13:54:46 -0700

    add proposal object

--- proto/collab/errors.go
+++ proto/collab/errors.go
@@ -5,4 +5,7 @@ import "errors"
 var (
 	ErrConcernAlreadyExists = errors.New("concern already exists")
 	ErrConcernAlreadyClosed = errors.New("concern already closed")
+
+	ErrProposalAlreadyExists = errors.New("proposal already exists")
+	ErrProposalAlreadyClosed = errors.New("proposal already closed")
 )

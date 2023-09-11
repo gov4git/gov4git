ad890479ff0a42a1aae16579b8376b9701ab8a73
Petar Maymounkov <petarm@gmail.com>
2023-09-11 10:13:25 -0700

    log gov4git version in verbose mode

--- TODO.md
+++ TODO.md
@@ -1,12 +1,7 @@
 
 FRAMEWORK
-- commit-level signatures
 - communication through cloning
 - track governance software code through commits hashes
 
 DOC
-- terminal-cast type video tutorials
 - documentation with mdBook https://github.com/rust-lang/mdBook
-
-FEATURES
-- semantic human-readable stack traces
--- gov4git/main.go
+++ gov4git/main.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"os"
 
+	gov4git_root "github.com/gov4git/gov4git"
 	"github.com/gov4git/gov4git/gov4git/cmd"
 	"github.com/gov4git/lib4git/base"
 	"github.com/gov4git/lib4git/must"
 )
 
 func main() {
+	base.Infof("gov4git version %v", gov4git_root.VersionDotTxt)
 	err, stk := must.TryWithStack(
 		func() { cmd.Execute() },
 	)

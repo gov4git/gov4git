919156e0d93ee4d6ccac9407becdb7c00dbc6ec0
Petar Maymounkov <petarm@gmail.com>
2023-10-24 09:16:39 -0700

    fix after rebase to v1.1.12

--- proto/ballot/common/schema_test.go
+++ proto/ballot/common/schema_test.go
@@ -2,12 +2,10 @@ package common
 
 import (
 	"testing"
-
-	"github.com/gov4git/lib4git/ns"
 )
 
 func TestBallotTopic(t *testing.T) {
-	testBallotName := ns.NS{"a", "b", "c"}
+	testBallotName := BallotName{"a", "b", "c"}
 	expBallotTopic := "ballot:a/b/c"
 	if BallotTopic(testBallotName) != expBallotTopic {
 		t.Errorf("expecting %v, got %v", expBallotTopic, BallotTopic(testBallotName))

b1f6dd1e2939d3b9404a4e915998eac4ea046e35
Petar Maymounkov <petarm@gmail.com>
2023-11-07 13:49:47 -0800

    add motion policy registry

--- proto/docket/policy/policy.go
+++ proto/docket/policy/policy.go
@@ -1,1 +1,17 @@
 package policy
+
+import "github.com/gov4git/gov4git/proto/mod"
+
+type Policy interface{
+	
+}
+
+var policyRegistry = mod.NewModuleRegistry[Policy]()
+
+func Set(key string, policy Policy) {
+	policyRegistry.Set(key, policy)
+}
+
+func Get(key string) Policy {
+	return policyRegistry.Get(key)
+}

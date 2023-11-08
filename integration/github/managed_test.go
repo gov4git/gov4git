ebab0fa717c15f47e126d17fd70a76873bef1924
Petar Maymounkov <petarm@gmail.com>
2023-11-08 10:17:02 -0800

    add integration test for syncing managed issues

--- integration/github/managed_test.go
+++ integration/github/managed_test.go
@@ -7,8 +7,10 @@ import (
 	"testing"
 
 	govgh "github.com/gov4git/gov4git/github"
+	"github.com/gov4git/gov4git/proto/docket/ops"
 	"github.com/gov4git/gov4git/runtime"
 	"github.com/gov4git/gov4git/test"
+	"github.com/gov4git/lib4git/form"
 	"github.com/gov4git/lib4git/testutil"
 )
 
@@ -24,32 +26,33 @@ func TestSyncManagedIssues(t *testing.T) {
 	// import issues
 	govgh.SyncManagedIssues(ctx, ghRepo, ghClient, cty.Organizer())
 
-	/*
-		// list ballots
-		ads1 := ballot.List(ctx, cty.Gov())
-		if len(ads1) < 4 {
-			t.Errorf("expecting at least 3, got %v,\n%v", len(ads1), form.SprintJSON(ads1))
-		}
-		// issue-1: open, not-frozen
-		if ads1[0].Name.GitPath() != "github/issues/1" {
-			t.Errorf("expecting github/issues/1, got %v", ads1[0].Name.GitPath())
-		}
-		if ads1[0].Closed || ads1[0].Frozen {
-			t.Errorf("expecting open, not-frozen")
-		}
-		// issue-2: open, frozen
-		if ads1[1].Name.GitPath() != "github/issues/2" {
-			t.Errorf("expecting github/issues/2, got %v", ads1[1].Name.GitPath())
-		}
-		if ads1[1].Closed || !ads1[1].Frozen {
-			t.Errorf("expecting open, frozen")
-		}
-		// issue-3: closed, frozen
-		if ads1[2].Name.GitPath() != "github/issues/3" {
-			t.Errorf("expecting github/issues/3, got %v", ads1[2].Name.GitPath())
-		}
-		if !ads1[2].Closed || !ads1[2].Frozen {
-			t.Errorf("expecting closed, frozen")
-		}
-	*/
+	// list motions
+	ms := ops.ListMotions(ctx, cty.Gov())
+	if len(ms) < 4 {
+		t.Errorf("expecting at least 4, got %v,\n%v", len(ms), form.SprintJSON(ms))
+	}
+
+	// issue-1: open, not-frozen
+	if ms[0].ID != "1" {
+		t.Errorf("expecting 1, got %v", ms[0].ID)
+	}
+	if ms[0].Closed || ms[0].Frozen {
+		t.Errorf("expecting open, not-frozen")
+	}
+
+	// issue-2: open, frozen
+	if ms[1].ID != "2" {
+		t.Errorf("expecting 2, got %v", ms[1].ID)
+	}
+	if ms[1].Closed || !ms[1].Frozen {
+		t.Errorf("expecting open, frozen")
+	}
+
+	// issue-3: closed, frozen
+	if ms[2].ID != "3" {
+		t.Errorf("expecting 3, got %v", ms[2].ID)
+	}
+	if !ms[2].Closed || !ms[2].Frozen {
+		t.Errorf("expecting closed, frozen")
+	}
 }

52b4f6278724a7a9f88d12ac73a50c88acf17c33
Petar Maymounkov <petarm@gmail.com>
2023-11-27 11:38:17 -0800

    assign unique ids to notices

--- proto/notice/schema.go
+++ proto/notice/schema.go
@@ -1,8 +1,12 @@
 package notice
 
 import (
+	"crypto/rand"
 	"fmt"
+	"strings"
 	"time"
+
+	"github.com/gov4git/lib4git/form"
 )
 
 type Notice struct {
@@ -18,6 +22,7 @@ func Noticef(format string, args ...any) Notices {
 type Notices []Notice
 
 type NoticeState struct {
+	ID        string    `json:"id"`
 	Stamp     time.Time `json:"stamp"`
 	Notice    Notice    `json:"notice"`
 	Displayed bool      `json:"shown"`
@@ -42,10 +47,18 @@ func NewNoticeQueue() *NoticeQueue {
 func (x *NoticeQueue) Append(notices ...Notice) {
 	for _, notice := range notices {
 		s := NoticeState{
+			ID:        GenerateRandomNoticeID(),
 			Stamp:     time.Now(),
 			Notice:    notice,
 			Displayed: false,
 		}
 		x.NoticeStates = append(x.NoticeStates, s)
 	}
+}
+
+func GenerateRandomNoticeID() string {
+	const w = 512 / 8 // 512 bits, measured in bytes
+	buf := make([]byte, w)
+	rand.Read(buf)
+	return strings.ToUpper(form.BytesHashForFilename(buf)[:6])
 }

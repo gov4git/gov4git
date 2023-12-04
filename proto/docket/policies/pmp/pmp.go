01ca6c35bafa3bfe49510a89310270f9dfaf1224
Petar Maymounkov <petarm@gmail.com>
2023-12-04 11:10:44 -0800

    create accounts for pmp tax pool and matching pool on boot

--- proto/docket/policies/pmp/pmp.go
+++ proto/docket/policies/pmp/pmp.go
@@ -2,8 +2,12 @@
 package pmp
 
 import (
+	"context"
+
+	"github.com/gov4git/gov4git/proto/account"
 	"github.com/gov4git/gov4git/proto/ballot/common"
 	"github.com/gov4git/gov4git/proto/docket/schema"
+	"github.com/gov4git/gov4git/proto/gov"
 )
 
 func ConcernPollBallotName(id schema.MotionID) common.BallotName {
@@ -12,4 +16,42 @@ func ConcernPollBallotName(id schema.MotionID) common.BallotName {
 
 func ProposalReferendumBallotName(id schema.MotionID) common.BallotName {
 	return common.BallotName{"pmp", "motion", "referendum", id.String()}
+}
+
+var (
+	OwnerID = account.AccountIDFromLine(
+		account.Term("pmp"),
+	)
+	TaxPoolAccountID = account.AccountIDFromLine(
+		account.Cat(
+			account.Term("pmp"),
+			account.Term("tax_pool"),
+		),
+	)
+	MatchingPoolAccountID = account.AccountIDFromLine(
+		account.Cat(
+			account.Term("pmp"),
+			account.Term("matching_pool"),
+		),
+	)
+)
+
+func Boot_StageOnly(ctx context.Context, cloned gov.Cloned) {
+
+	// create tax pool account
+	account.Create_StageOnly(
+		ctx,
+		cloned,
+		TaxPoolAccountID,
+		account.OwnerID(OwnerID),
+	)
+
+	// create matching pool account
+	account.Create_StageOnly(
+		ctx,
+		cloned,
+		MatchingPoolAccountID,
+		account.OwnerID(OwnerID),
+	)
+
 }

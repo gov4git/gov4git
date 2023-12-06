1dade9eb342dfbf2ff16aee837ce246f1f4b9b5c
Petar Maymounkov <petarm@gmail.com>
2023-12-06 10:22:34 -0800

    add motion zero edit test

--- proto/docket/ops/edit.go
+++ proto/docket/ops/edit.go
@@ -4,17 +4,36 @@ import (
 	"context"
 	"slices"
 
+	"github.com/gov4git/gov4git/proto"
 	"github.com/gov4git/gov4git/proto/docket/schema"
+	"github.com/gov4git/gov4git/proto/gov"
 	"github.com/gov4git/lib4git/git"
+	"github.com/gov4git/lib4git/must"
 )
+
+func EditMotion(
+	ctx context.Context,
+	addr gov.OwnerAddress,
+	id schema.MotionID,
+	title string,
+	body string,
+	trackerURL string,
+	labels []string,
+
+) git.ChangeNoResult {
+
+	cloned := gov.CloneOwner(ctx, addr)
+	chg := EditMotionMeta_StageOnly(ctx, cloned, id, title, body, trackerURL, labels)
+	return proto.CommitIfChanged(ctx, cloned.PublicClone(), chg)
+}
 
 func EditMotionMeta_StageOnly(
 	ctx context.Context,
-	t *git.Tree,
+	cloned gov.OwnerCloned,
 	id schema.MotionID,
-	trackerURL string,
 	title string,
-	desc string,
+	body string,
+	trackerURL string,
 	labels []string,
 
 ) git.ChangeNoResult {
@@ -22,10 +41,12 @@ func EditMotionMeta_StageOnly(
 	labels = slices.Clone(labels)
 	slices.Sort(labels)
 
-	motion := schema.MotionKV.Get(ctx, schema.MotionNS, t, id)
+	motion := schema.MotionKV.Get(ctx, schema.MotionNS, cloned.PublicClone().Tree(), id)
+	must.Assertf(ctx, !motion.Closed, "cannot edit a closed motion %v", id)
+
 	motion.TrackerURL = trackerURL
 	motion.Title = title
-	motion.Body = desc
+	motion.Body = body
 	motion.Labels = labels
-	return schema.MotionKV.Set(ctx, schema.MotionNS, t, id, motion)
+	return schema.MotionKV.Set(ctx, schema.MotionNS, cloned.PublicClone().Tree(), id, motion)
 }
---
+++ test/docket/zero/edit_test.go
@@ -0,0 +1,51 @@
+package zero
+
+import (
+	"testing"
+
+	"github.com/gov4git/gov4git/proto/docket/ops"
+	"github.com/gov4git/gov4git/proto/docket/policies/zero"
+	"github.com/gov4git/gov4git/proto/docket/schema"
+	"github.com/gov4git/gov4git/runtime"
+	"github.com/gov4git/gov4git/test"
+	"github.com/gov4git/lib4git/base"
+	"github.com/gov4git/lib4git/testutil"
+)
+
+func TestEdit(t *testing.T) {
+	base.LogVerbosely()
+	ctx := testutil.NewCtx(t, runtime.TestWithCache)
+	cty := test.NewTestCommunity(t, ctx, 2)
+
+	id := schema.MotionID("123")
+
+	// open
+	ops.OpenMotion(
+		ctx,
+		cty.Organizer(),
+		id,
+		zero.ZeroPolicyName,
+		cty.MemberUser(0),
+		"concern #1",
+		"description #1",
+		schema.MotionConcernType,
+		"https://1",
+		nil)
+
+	// edit
+	ops.EditMotion(ctx, cty.Organizer(), id, "concern #2", "description #2", "https://2", nil)
+
+	// verify state changed
+	m := ops.ShowMotion(ctx, cty.Gov(), id)
+	if m.Motion.Title != "concern #2" {
+		t.Errorf("expecting %v, got %v", "concern #2", m.Motion.Title)
+	}
+	if m.Motion.Body != "description #2" {
+		t.Errorf("expecting %v, got %v", "description #2", m.Motion.Body)
+	}
+	if m.Motion.TrackerURL != "https://2" {
+		t.Errorf("expecting %v, got %v", "https://2", m.Motion.TrackerURL)
+	}
+
+	// testutil.Hang()
+}

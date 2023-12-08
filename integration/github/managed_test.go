308c1f28631399d3c20ed058880a830df213a6ce
Petar Maymounkov <petarm@gmail.com>
2023-12-07 22:32:53 -0800

    add integration test for managed issues/prs

--- integration/github/managed_test.go
+++ integration/github/managed_test.go
@@ -8,6 +8,7 @@ import (
 
 	govgh "github.com/gov4git/gov4git/github"
 	"github.com/gov4git/gov4git/proto/docket/ops"
+	"github.com/gov4git/gov4git/proto/docket/schema"
 	"github.com/gov4git/gov4git/runtime"
 	"github.com/gov4git/gov4git/test"
 	"github.com/gov4git/lib4git/form"
@@ -24,13 +25,16 @@ func TestSyncManagedIssues(t *testing.T) {
 	cty := test.NewTestCommunity(t, ctx, 2)
 
 	// import issues
-	govgh.SyncManagedIssues(ctx, ghRepo, ghClient, cty.Organizer())
+	syncChanges := govgh.SyncManagedIssues(ctx, ghRepo, ghClient, cty.Organizer())
+	_ = syncChanges
+	// fmt.Println(form.SprintJSON(syncChanges))
 
 	// list motions
 	ms := ops.ListMotions(ctx, cty.Gov())
 	if len(ms) < 4 {
-		t.Errorf("expecting at least 4, got %v,\n%v", len(ms), form.SprintJSON(ms))
+		t.Errorf("expecting at least 4 got %v,\n%v", len(ms), form.SprintJSON(ms))
 	}
+	// fmt.Println(form.SprintJSON(ms))
 
 	// issue-1: open, not-frozen
 	if ms[0].ID != "1" {
@@ -38,6 +42,14 @@ func TestSyncManagedIssues(t *testing.T) {
 	}
 	if ms[0].Closed || ms[0].Frozen {
 		t.Errorf("expecting open, not-frozen")
+	}
+	// refs
+	if len(ms[0].RefBy) != 1 {
+		t.Errorf("expecting %v, got %v", 1, ms[0].RefBy)
+	}
+	ms0RefBy0 := schema.Ref{Type: "addresses", From: schema.MotionID("5"), To: schema.MotionID("1")}
+	if ms[0].RefBy[0] != ms0RefBy0 {
+		t.Errorf("expecting %v, got %v", ms0RefBy0, ms[0].RefBy[0])
 	}
 
 	// issue-2: open, frozen
@@ -47,12 +59,52 @@ func TestSyncManagedIssues(t *testing.T) {
 	if ms[1].Closed || !ms[1].Frozen {
 		t.Errorf("expecting open, frozen")
 	}
-
-	// issue-3: closed, frozen
-	if ms[2].ID != "3" {
-		t.Errorf("expecting 3, got %v", ms[2].ID)
+	// refs
+	if len(ms[1].RefBy) != 2 {
+		t.Errorf("expecting %v, got %v", 2, ms[1].RefBy)
 	}
-	if !ms[2].Closed || !ms[2].Frozen {
-		t.Errorf("expecting closed, frozen")
+	ms1RefBy0 := schema.Ref{Type: "addresses", From: schema.MotionID("5"), To: schema.MotionID("2")}
+	ms1RefBy1 := schema.Ref{Type: "addresses", From: schema.MotionID("7"), To: schema.MotionID("2")}
+	if ms[1].RefBy[0] != ms1RefBy0 {
+		t.Errorf("expecting %v, got %v", ms1RefBy0, ms[1].RefBy[0])
+	}
+	if ms[1].RefBy[1] != ms1RefBy1 {
+		t.Errorf("expecting %v, got %v", ms1RefBy1, ms[1].RefBy[1])
+	}
+
+	// issue-5: closed, frozen
+	if ms[2].ID != "5" {
+		t.Errorf("expecting 5, got %v", ms[2].ID)
+	}
+	if ms[2].Closed || ms[2].Frozen {
+		t.Errorf("expecting open, not frozen")
+	}
+	// refs
+	if len(ms[2].RefTo) != 2 {
+		t.Errorf("expecting %v, got %v", 2, ms[2].RefTo)
+	}
+	ms2RefTo0 := schema.Ref{Type: "addresses", From: schema.MotionID("5"), To: schema.MotionID("1")}
+	ms2RefTo1 := schema.Ref{Type: "addresses", From: schema.MotionID("5"), To: schema.MotionID("2")}
+	if ms[2].RefTo[0] != ms2RefTo0 {
+		t.Errorf("expecting %v, got %v", ms2RefTo0, ms[2].RefTo[0])
+	}
+	if ms[2].RefTo[1] != ms2RefTo1 {
+		t.Errorf("expecting %v, got %v", ms2RefTo1, ms[2].RefTo[1])
+	}
+
+	// issue-7: closed, frozen
+	if ms[3].ID != "7" {
+		t.Errorf("expecting 7, got %v", ms[3].ID)
+	}
+	if ms[3].Closed || ms[3].Frozen {
+		t.Errorf("expecting open, not frozen")
+	}
+	// refs
+	if len(ms[3].RefTo) != 1 {
+		t.Errorf("expecting %v, got %v", 1, ms[3].RefTo)
+	}
+	ms3RefTo0 := schema.Ref{Type: "addresses", From: schema.MotionID("7"), To: schema.MotionID("2")}
+	if ms[3].RefTo[0] != ms3RefTo0 {
+		t.Errorf("expecting %v, got %v", ms3RefTo0, ms[3].RefTo[0])
 	}
 }

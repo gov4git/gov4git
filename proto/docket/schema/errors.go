2e84afad80d53961da7d425ed0e531f7ff02d97a
Petar Maymounkov <petarm@gmail.com>
2023-12-14 12:04:37 -0800

    improve error msg

--- proto/docket/schema/errors.go
+++ proto/docket/schema/errors.go
@@ -3,9 +3,10 @@ package schema
 import "errors"
 
 var (
-	ErrMotionAlreadyExists = errors.New("motion already exists")
-	ErrMotionAlreadyClosed = errors.New("motion already closed")
-	ErrMotionNotClosed     = errors.New("motion is not closed")
-	ErrMotionAlreadyFrozen = errors.New("motion already frozen")
-	ErrMotionNotFrozen     = errors.New("motion is not frozen")
+	ErrMotionAlreadyExists    = errors.New("motion already exists")
+	ErrMotionAlreadyClosed    = errors.New("motion already closed")
+	ErrMotionAlreadyCancelled = errors.New("motion already cancelled")
+	ErrMotionNotClosed        = errors.New("motion is not closed")
+	ErrMotionAlreadyFrozen    = errors.New("motion already frozen")
+	ErrMotionNotFrozen        = errors.New("motion is not frozen")
 )

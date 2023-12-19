3ab52e4b6d0710cc1d046cdd3ad84aa08c368293
Petar Maymounkov <petarm@gmail.com>
2023-12-19 13:04:58 -0800

    upgrade github repo url parsing tools; add upload assets routine

--- github/repo_test.go
+++ github/repo_test.go
@@ -14,3 +14,16 @@ func TestParseGithubRepoHTTPSURL(t *testing.T) {
 		t.Errorf("expecting %v, got %v", "xyz", repo.Name)
 	}
 }
+
+func TestParseGithubRepoSSHURL(t *testing.T) {
+	repo, err := parseGithubRepoSSHURL("git@github.com:abc/x.y.z.git")
+	if err != nil {
+		t.Error(err)
+	}
+	if repo.Owner != "abc" {
+		t.Errorf("expecting %v, got %v", "abc", repo.Owner)
+	}
+	if repo.Name != "x.y.z" {
+		t.Errorf("expecting %v, got %v", "x.y.z", repo.Name)
+	}
+}

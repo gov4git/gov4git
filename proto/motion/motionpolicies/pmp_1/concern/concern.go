eb4030ff2dbe7fd0d1103c9024558f93a9488258
Petar Maymounkov <petarm@gmail.com>
2024-01-26 11:45:46 -0800

    fix bug in concern close

--- proto/motion/motionpolicies/pmp_1/concern/concern.go
+++ proto/motion/motionpolicies/pmp_1/concern/concern.go
@@ -246,7 +246,7 @@ func (x concernPolicy) Close(
 	must.Assertf(ctx, ok, "unrecognized proposal motion argument %v", args[1])
 
 	// update the policy state before closing the motion
-	x.Update(ctx, cloned, prop, policyNS)
+	x.Update(ctx, cloned, con, policyNS)
 
 	// close the poll for the motion
 	priorityPollName := pmp_1.ConcernPollBallotName(con.ID)

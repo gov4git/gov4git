7174d10b9568a397a8600c8daa29382abebb9ba6
Petar Maymounkov <petarm@gmail.com>
2024-01-30 13:59:41 -0800

    print stack trace to stderr with -v flag

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/go-git/go-billy/v5 v5.5.0
 	github.com/go-git/go-git/v5 v5.10.0
 	github.com/google/go-github/v55 v55.0.0
-	github.com/gov4git/lib4git v0.0.28
+	github.com/gov4git/lib4git v0.0.29
 	github.com/gov4git/vendor4git v0.0.3
 	github.com/migueleliasweb/go-github-mock v0.0.19
 	github.com/rogpeppe/go-internal v1.11.0
--- gov4git/api/result.go
+++ gov4git/api/result.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/gov4git/lib4git/base"
 	"github.com/gov4git/lib4git/form"
 	"github.com/gov4git/lib4git/must"
 )
@@ -15,14 +16,21 @@ type Result struct {
 }
 
 func Invoke(f func()) Result {
-	r := NewResult(nil, must.Try(f))
+	err := must.TryThru(f)
+	r := NewResult(nil, err.Wrapped())
+	if err != nil && base.IsVerbose() {
+		fmt.Fprint(os.Stderr, string(err.Stack))
+	}
 	fmt.Fprint(os.Stdout, form.SprintJSON(r))
 	return r
 }
 
 func Invoke1[R1 any](f func() R1) Result {
-	r1, err := must.Try1[R1](f)
-	r := NewResult(r1, err)
+	r1, err := must.Try1Thru[R1](f)
+	r := NewResult(r1, err.Wrapped())
+	if err != nil && base.IsVerbose() {
+		fmt.Fprint(os.Stderr, string(err.Stack))
+	}
 	fmt.Fprint(os.Stdout, form.SprintJSON(r))
 	return r
 }

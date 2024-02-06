fe70dbfb63c0708cdf090979ad2b946583907882
Petar Maymounkov <petarm@gmail.com>
2024-02-05 16:49:37 -0800

    add policy use packages

---
+++ proto/motion/motionpolicies/pmp_0/use/use.go
@@ -0,0 +1,6 @@
+package use
+
+import (
+	_ "github.com/gov4git/gov4git/v2/proto/motion/motionpolicies/pmp_0/concern"
+	_ "github.com/gov4git/gov4git/v2/proto/motion/motionpolicies/pmp_0/proposal"
+)
